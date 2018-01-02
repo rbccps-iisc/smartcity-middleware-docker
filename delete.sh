300f3190d9754259757b617b0a8017bf85b2ee4d
Harish Anand <harishanand95@gmail.com>
2018-01-02 13:51:46 +0530

    Fix: add autoclean, remove ldap data dirs

--- delete.sh
+++ delete.sh
@@ -2,7 +2,6 @@
 cd /data/kong && sudo rm -rf *
 cd /data/tomcat && sudo rm -rf *
 cd /data/rabbitmq && sudo rm -rf *
-cd /data/ldap && sudo rm -rf *
 cd /data/logs/kong && sudo rm -rf *
 cd /data/logs/rabbitmq && sudo rm -rf *
 cd /data/logs/tomcat && sudo rm -rf *
