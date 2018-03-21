985413d39a3f04e1bdd92cc7eb9e6568ffcc2dd2
Harish Anand <harishanand95@gmail.com>
2018-03-21 14:07:39 +0530

    Fix: missing requirements

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update && sudo apt-get install -y software-properties-common && sudo apt-add-repository ppa:ansible/ansible -y && sudo apt-get -y update && sudo apt-get install -y ansible
 sudo apt-get install -y apt-transport-https  ca-certificates curl software-properties-common
 sudo apt install python-pip python -y
-python -m pip install pathlib2
+python -m pip install pathlib2 passlib
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update -y
@@ -20,3 +20,4 @@ sudo usermod -aG docker $USER
 ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
 sudo chmod -R 777 /data/*
 sudo sysctl -w vm.max_map_count=662144
+
