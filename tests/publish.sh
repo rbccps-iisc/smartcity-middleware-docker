9a3f83d73ccbbf433ecff5798abd9ef7e1d9cb55
Harish Anand <harishanand95@gmail.com>
2018-03-27 12:29:33 +0530

    tests: pub latency in Google Cloud Engine (travis)
    Signed-off-by: Harish Anand <harishanand95@gmail.com>

--- tests/publish.sh
+++ tests/publish.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+i="0"
+while [ $i -lt 10000000 ]
+do
 curl -ik -X POST https://localhost:10443/api/1.0.0/publish -H "apikey: $1" -d '{"body": "travis test"}'
+i=$[$i+1]
+done
--- tests/pytest.py
+++ tests/pytest.py
@@ -1,5 +1,6 @@
 import subprocess
 import json
+import time
 
 register = subprocess.check_output("./tests/create_entity.sh streetlight", shell=True)
 print(register)
@@ -11,4 +12,8 @@ print(register)
 register = json.loads(register)
 dashboard_key = register["apiKey"]
 print(dashboard_key)
+
+start = time.time()
 publish = subprocess.check_output("./tests/publish.sh "+streetlight_key, shell=True)
+end = time.time()
+print(end - start)
