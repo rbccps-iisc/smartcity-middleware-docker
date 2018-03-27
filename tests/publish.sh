7c6a5d4ddb292ec7a5e1d419be0bd32d0c4862e9
Harish Anand <harishanand95@gmail.com>
2018-03-27 20:21:05 +0530

    Fix: reduce publish to million requests

--- tests/publish.sh
+++ tests/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 i="0"
-while [ $i -lt 10000000 ]
+while [ $i -lt 1000000 ]
 do
 curl -ik -X POST https://localhost:10443/api/1.0.0/publish -H "apikey: $1" -d '{"body": "travis test"}'
 i=$[$i+1]
