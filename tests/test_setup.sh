308fd9bc045bba31bef5db973bd98502acb8b495
Harish Anand <harishanand95@gmail.com>
2018-04-01 00:23:48 +0530

    Fix: avoid pathlib requirement, use ideam.conf

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -5,8 +5,7 @@ sudo dpkg -i ideam_0.0-1.deb
 sudo dpkg --remove ideam
 sudo apt-get -y update && sudo apt-get install -y software-properties-common && sudo apt-add-repository ppa:ansible/ansible -y && sudo apt-get -y update && sudo apt-get install -y ansible
 sudo apt-get install -y apt-transport-https  ca-certificates curl software-properties-common
-sudo apt install python-pip python -y
-python -m pip install pathlib2
+sudo apt install python -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update -y
