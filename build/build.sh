ebf6b045dffd3b37faefe3e1d71e0e19e2d4f9b7
Harish Anand <harishanand95@gmail.com>
2018-04-29 10:57:47 +0530

    deb: auto installation of ansible and docker
    Signed-off-by: Harish Anand <harishanand95@gmail.com>

--- build/build.sh
+++ build/build.sh
@@ -16,6 +16,7 @@ mkdir -p ideam_0.0-1/var/ideam/data/logs/rabbitmq
 mkdir -p ideam_0.0-1/var/ideam/data/logs/tomcat
 cp ../ideam.py ideam_0.0-1/usr/local/bin/ideam
 cp debian/control ideam_0.0-1/DEBIAN/control
+cp debian/postinst ideam_0.0-1/DEBIAN/postinst
 cp ../ideam.conf ideam_0.0-1/etc/ideam/
 chmod +x ideam_0.0-1/usr/local/bin/ideam
 cd ../
