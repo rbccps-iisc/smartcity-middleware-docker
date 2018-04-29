6f6105dce3c131021c8e497a66c76dd1f24a3ad5
Harish Anand <harishanand95@gmail.com>
2018-04-29 11:15:26 +0530

    Fix: call preinst instead of postinst

--- build/build.sh
+++ build/build.sh
@@ -16,8 +16,8 @@ mkdir -p ideam_0.0-1/var/ideam/data/logs/rabbitmq
 mkdir -p ideam_0.0-1/var/ideam/data/logs/tomcat
 cp ../ideam.py ideam_0.0-1/usr/local/bin/ideam
 cp debian/control ideam_0.0-1/DEBIAN/control
-cp debian/postinst ideam_0.0-1/DEBIAN/postinst
-chmod 555 ideam_0.0-1/DEBIAN/postinst
+cp debian/preinst ideam_0.0-1/DEBIAN/preinst
+chmod 555 ideam_0.0-1/DEBIAN/preinst
 cp ../ideam.conf ideam_0.0-1/etc/ideam/
 chmod +x ideam_0.0-1/usr/local/bin/ideam
 cd ../
