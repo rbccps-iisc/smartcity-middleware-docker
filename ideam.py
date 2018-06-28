241f33eef15e090ef65f4939f29e64c5d4e43556
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-06-28 15:43:28 +0530

    Bugfix: Updated ideam.py

--- ideam.py
+++ ideam.py
@@ -105,6 +105,9 @@ def test(arguments):
     except:
         output_error(process,
                      error_message=traceback.format_exc())
+        f=open("/tmp/status")
+        print(f.read())
+        f.close()
         exit()
 
     cmd = "./tests/create_entity.sh apitestingdashboard"
