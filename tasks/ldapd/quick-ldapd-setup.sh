6043bd03775ce950517f351fb63e8bdb33c3a4d3
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-07-02 17:14:41 +0530

    Added delay for LDAPD to start

--- tasks/ldapd/quick-ldapd-setup.sh
+++ tasks/ldapd/quick-ldapd-setup.sh
@@ -3,6 +3,12 @@ echo "TrustedUserCAKeys /etc/ssh/ca-user-certificate-key.pub" >> /etc/ssh/sshd_c
 pwd=`cat /etc/ldapd | cut -d : -f 2 | awk '{$1=$1};1'`
 sed -i 's/ldap_pwd/'$pwd'/g' /etc/ldapd.conf
 /usr/local/sbin/ldapd
+
+while ! nc -z localhost 8389
+do
+sleep 0.1
+done
+
 ldapmodify -h 127.0.0.1 -p 8389 -x -D cn=admin,dc=smartcity -w $pwd -f /smartcity.ldif
 ldapmodify -h 127.0.0.1 -p 8389 -x -D cn=admin,dc=smartcity -w $pwd -f /devices.ldif
 rm /etc/ldapd
