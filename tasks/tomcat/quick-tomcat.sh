59282d45eb79f8f5dd35a17be14cb2b7ad90eba2
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-07-02 17:00:36 +0530

    Updated war file location

--- tasks/tomcat/quick-tomcat.sh
+++ tasks/tomcat/quick-tomcat.sh
@@ -3,7 +3,7 @@ docker cp config/certificate_authority/keys/ca-user-certificate-key.pub tomcat:/
 docker exec -i tomcat mkdir -p /root/.ssh/ 
 docker exec -i tomcat dd of=/root/.ssh/authorized_keys < ~/.ssh/id_rsa.pub > /dev/null 2>&1
 #sshpass -p "rbccps@123456" ssh root@localhost -p $1 < tasks/tomcat/quick-tomcat-setup.sh 
-docker cp config/tomcat/RegisterAPI.war tomcat:/usr/local/webapps 
+docker cp config/tomcat/RegisterAPI.war tomcat:/usr/local/tomcat/webapps 
 docker cp tasks/tomcat/quick-tomcat-setup.sh tomcat:/etc/
 docker exec tomcat chmod +x /etc/quick-tomcat-setup.sh
 docker exec tomcat /etc/quick-tomcat-setup.sh 
