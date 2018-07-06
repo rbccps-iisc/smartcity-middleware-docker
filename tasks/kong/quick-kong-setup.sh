bf50393f79682ffc6a02f070c97931579fdd2f05
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-07-06 12:12:42 +0530

    Cleared old tmux cache

--- tasks/kong/quick-kong-setup.sh
+++ tasks/kong/quick-kong-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
 echo "TrustedUserCAKeys /etc/ssh/ca-user-certificate-key.pub" >> /etc/ssh/sshd_config
+rm -r /tmp/tmux-*
 
 su postgres -c "/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data > /var/lib/postgresql/logfile 2>&1 &"
 
