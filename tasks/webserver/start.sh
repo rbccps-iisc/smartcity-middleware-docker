04255c660c2184ed58322308c7dc55de5ec4f945
vasanth <swaminathanvasanth.r@gmail.com>
2018-08-07 15:03:38 +0530

    Removed tmux : Fixed webserver nohup start issue

--- tasks/webserver/start.sh
+++ tasks/webserver/start.sh
@@ -10,7 +10,7 @@ then
 
 echo -e "${YELLOW}[  INFO  ]${NC} Starting Webserver"
 
-tmux new-session -d -s my_session 'java -jar /usr/local/webserver/cdxapis.jar'
+nohup java -jar /usr/local/webserver/cdxapis.jar 2>/dev/null >/dev/null &
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[   OK   ]${NC} Started Webserver"
