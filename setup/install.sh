fd0a9d3ffd11a26d0aa481099827036f6d3e8ff9
vasanth <swaminathanvasanth.r@gmail.com>
2018-08-30 21:39:23 +0530

    Adding database subscriber setup in initial install script

--- setup/install.sh
+++ setup/install.sh
@@ -12,7 +12,7 @@ echo -e "${YELLOW}[  INFO  ]${NC} Creating admin.ideam user"
 touch /usr/local/kong/consumer_error.log
 touch /usr/local/kong/consumer_out.log
 
-sh /usr/local/kong/setup/setup_consumer.sh admin.ideam 2>/usr/local/kong.consumer_error.log >/usr/local/kong/consumer_out.log
+sh /usr/local/kong/setup/setup_consumer.sh admin.ideam 2>/usr/local/kong/consumer_error.log >/usr/local/kong/consumer_out.log
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[   OK   ] ${NC} Created admin.ideam user"
@@ -24,7 +24,7 @@ echo -e "${YELLOW}[  INFO  ]${NC} Adding ACL to admin.ideam user"
 
 touch /usr/local/kong/acl_error.log
 touch /usr/local/kong/acl_out.log
-sh /usr/local/kong/setup/setup_consumer-acl.sh admin.ideam 2>/usr/local/kong.acl_error.log >/usr/local/kong/acl_out.log
+sh /usr/local/kong/setup/setup_consumer-acl.sh admin.ideam 2>/usr/local/kong/acl_error.log >/usr/local/kong/acl_out.log
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[   OK   ] ${NC} Added ACL to admin.ideam user"
@@ -37,13 +37,11 @@ echo -e "${YELLOW}[  INFO  ]${NC} Adding key-auth to admin.ideam user"
 touch /usr/local/kong/auth_error.log
 touch /usr/local/kong/auth_out.log
 
-sh /usr/local/kong/setup/setup_consumer-key-auth.sh admin.ideam 2>/usr/local/kong.auth_error.log >/usr/local/kong/auth.log
+sh /usr/local/kong/setup/setup_consumer-key-auth.sh admin.ideam 2>/usr/local/kong/auth_error.log >/usr/local/kong/auth_out.log
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[   OK   ] ${NC}Added key-auth to admin.ideam user"
 else
     echo -e "${RED}[ ERROR ] ${NC}Failed to add key-auth to admin.ideam user"
 fi
-
-
 
